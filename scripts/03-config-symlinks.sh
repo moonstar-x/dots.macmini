afb3ec06e6dcc1e866e885abc06d72177ddfaee6
moonstar-x <christian.lopez99@outlook.com>
2026-08-27 18:05:10 -0500

    Added configs for ghostty, starship, and btop.

--- scripts/03-config-symlinks.sh
+++ scripts/03-config-symlinks.sh
@@ -4,9 +4,24 @@ SCRIPTS_DIR="$(pwd)"
 DOTFILES_ROOT="$(realpath "$SCRIPTS_DIR/..")"
 CONFIG_DIR="$DOTFILES_ROOT/config"
 
+MAC_XDG_CONFIG_HOME="$HOME/Library/Application Support"
+LINUX_XDG_CONFIG_HOME="$HOME/.config"
+
+echo "Linking config for btop"
+mkdir -p $LINUX_XDG_CONFIG_HOME/btop/themes
+rm -rf $LINUX_XDG_CONFIG_HOME/btop/btop.conf $LINUX_XDG_CONFIG_HOME/btop/themes
+ln -sf "$CONFIG_DIR/btop/btop.conf" "$LINUX_XDG_CONFIG_HOME/btop/btop.conf"
+ln -sf "$CONFIG_DIR/btop/themes" "$LINUX_XDG_CONFIG_HOME/btop/themes"
+
+echo "Linking config for ghostty"
+ln -sf "$CONFIG_DIR/ghostty/config" "$MAC_XDG_CONFIG_HOME/com.mitchellh.ghostty/config"
+
 echo "Linking config for git"
 ln -sf "$CONFIG_DIR/git/.gitconfig" "$HOME/.gitconfig"
 ln -sf "$CONFIG_DIR/git/.gitignore_global" "$HOME/.gitignore_global"
+
+echo "Linking config for starship"
+ln -sf "$CONFIG_DIR/starship/config.toml" "$LINUX_XDG_CONFIG_HOME/starship.toml"
 
 echo "Linking config for zsh"
 ln -sf "$CONFIG_DIR/zsh/.zprofile" "$HOME/.zprofile"
